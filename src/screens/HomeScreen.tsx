8e8ecc918bdc5124f48708d9b0f3a0bfe35a3c95
MDKoster <120367108+MDKoster@users.noreply.github.com>
2024-05-09 21:50:51 +0200

    Homescreen functionalities

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,13 +2,12 @@ import React, { useRef, useEffect, useState } from "react";
 import {
   ScrollView,
   StyleSheet,
-  View,
   Image,
   Dimensions,
-  FlatList,
-  Text,
-  TouchableOpacity,
   Animated,
+  Easing,
+  View,
+  TextInput,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeSuggestionListComponent from "../components/HomeSuggestionListComponent";
@@ -16,8 +15,9 @@ import HomeSuggestionListComponent from "../components/HomeSuggestionListCompone
 const HomeScreen = () => {
   const windowWidth = Dimensions.get("window").width;
   const scrollViewRef = useRef<ScrollView>(null);
+  const suggestionRef = useRef<ScrollView>(null);
   const [currentX, setCurrentX] = useState(0);
-  const [suggestionViewFlexValue, setSuggestionViewFlexValue] = useState(1);
+  const [delayScroll, setDelayScroll] = useState(true);
   const [images, setImages] = useState([
     require("../../assets/images/female-outfit-1.jpg"),
     require("../../assets/images/female-dress.jpg"),
@@ -32,7 +32,6 @@ const HomeScreen = () => {
       if (scrollViewRef.current !== null) {
         let nextX = 0;
         if (currentX + 1 < 3) nextX = currentX + 1;
-        // const nextX = currentX + 1 < 3 ? currentX + 1 : 0;
         scrollViewRef.current.scrollTo({
           x: windowWidth * nextX,
           animated: true,
@@ -44,47 +43,73 @@ const HomeScreen = () => {
     return () => clearInterval(scrollInterval);
   }, [currentX]);
 
-  const flexValue = new Animated.Value(1);
-  const toggleFlex = () => {
-    const newValue = suggestionViewFlexValue === 1 ? 2 : 1;
+  //TODO: don't do an animation from 1 to 2, register the y-offset value of every scroll event
+  // and add it to the height of the parent view (up to a certain limit) when the user scrolls down
+  // When the user scrolls back up, the height of the parent view should decrease only when the scrollview
+  // reaches the top
+  const flexValue = useRef(new Animated.Value(1)).current;
+  const toggleFlex = (targetValue: number) => {
     Animated.timing(flexValue, {
-      toValue: newValue,
-      duration: 500,
+      toValue: targetValue,
+      duration: 150,
       useNativeDriver: false,
-    }).start();
-    setSuggestionViewFlexValue(newValue);
+      easing: Easing.linear,
+    }).start(({ finished }) => {
+      finished
+        ? (flexValue.setValue(targetValue),
+          targetValue === 2 && setDelayScroll(true))
+        : null;
+    });
   };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <ScrollView
-        ref={scrollViewRef}
-        style={{ flex: 1 }}
-        horizontal={true}
-        pagingEnabled={true}
-        showsHorizontalScrollIndicator={false}
-        onScroll={(event) => {
-          setCurrentX(
-            Math.round(event.nativeEvent.contentOffset.x / windowWidth)
-          );
-        }}
-      >
-        <Image
-          source={require("../../assets/images/female-summer-banner.jpg")}
-          style={{ width: windowWidth, height: 350 }}
-        />
-        <Image
-          source={require("../../assets/images/urban-stylish-male.jpg")}
-          style={{ width: windowWidth, height: 350 }}
-        />
-        <Image
-          source={require("../../assets/images/outdoors-banner.jpg")}
-          style={{ width: windowWidth, height: 350 }}
-        />
-      </ScrollView>
-      <View
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <TextInput
+          style={{
+            position: "absolute",
+            top: 30,
+            zIndex: 1,
+            opacity: 0.8,
+            textAlign: "center",
+            paddingHorizontal: 10,
+            backgroundColor: "white",
+            width: 200,
+            height: 40,
+            borderRadius: 10,
+          }}
+          placeholder="Search"
+        ></TextInput>
+        <ScrollView
+          ref={scrollViewRef}
+          style={{ flex: 1 }}
+          horizontal={true}
+          pagingEnabled={true}
+          showsHorizontalScrollIndicator={false}
+          //The currentX state is updated when the user scrolls horizontally so the correct image is displayed (and the others that follow)
+          onScroll={(event) => {
+            setCurrentX(
+              Math.round(event.nativeEvent.contentOffset.x / windowWidth)
+            );
+          }}
+        >
+          <Image
+            source={require("../../assets/images/female-summer-banner.jpg")}
+            style={{ width: windowWidth, height: 350 }}
+          />
+          <Image
+            source={require("../../assets/images/urban-stylish-male.jpg")}
+            style={{ width: windowWidth, height: 350 }}
+          />
+          <Image
+            source={require("../../assets/images/outdoors-banner.jpg")}
+            style={{ width: windowWidth, height: 350 }}
+          />
+        </ScrollView>
+      </View>
+      <Animated.View
         style={{
-          flex: suggestionViewFlexValue,
+          flex: flexValue,
           backgroundColor: "white",
           borderTopLeftRadius: 25,
           borderTopRightRadius: 25,
@@ -93,9 +118,13 @@ const HomeScreen = () => {
         }}
       >
         <ScrollView
+          ref={suggestionRef}
+          disableScrollViewPanResponder={delayScroll}
+          //toggleFlex changes the height of the parent view.
+          //The direction of the scroll is checked to determine the height of the parent view.
+          //Y-offset bigger than 0 means the user is scrolling down (moving thumb up), and vice versa
           onScroll={(event) => {
-            //TODO: Fix scroll event
-            toggleFlex();
+            toggleFlex(event.nativeEvent.contentOffset.y > 0 ? 2 : 1);
           }}
         >
           <HomeSuggestionListComponent images={images} title="Popular" />
@@ -106,7 +135,7 @@ const HomeScreen = () => {
             title="Women's Fashion"
           />
         </ScrollView>
-      </View>
+      </Animated.View>
     </SafeAreaView>
   );
 };
